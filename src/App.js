1d9e3ac5333019f497a72b12de517418da3cb772
omidhldn <omidhldn@gmail.com>
2023-05-22 18:45:39 +0330

    first complete version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import Pray from './components/Pray';
+
+const DUMMY__INFO = [
+  {
+    title:"صبح",
+    bound:"2"
+  },
+  {
+    title:"ظهر",
+    bound:"4"
+  },
+  {
+    title:"عصر",
+    bound:"4"
+  },
+  {
+    title:"مغرب",
+    bound:"3"
+  },
+  {
+    title:"عشاء",
+    bound:"4"
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. created by omidhldn test2
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      {/* <Pray title="صبح" bound='2'/> */}
+      {/* <Pray data={DUMMY__INFO[0]}/> */}
+      {DUMMY__INFO.map((el,index) => <Pray key={index} data={el}/>)}
     </div>
   );
 }
---
+++ src/components/Pray.js
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import { ReactComponent as Smile } from './assets/emoji-smile.svg';
+import classes from './Pray.module.css';
+
+const Pray = (props) => {
+  const [rakat, setRakat] = useState(0);
+
+  const incrementHandler = (event) => {
+    event.preventDefault();
+    setRakat(prevState => prevState + 1);
+  };
+  return(
+    <form className={classes.container}>
+      <label>{props.data.title}</label>
+      <input type="text" value={`${rakat} / ${props.data.bound}`}></input>
+      <button type='button' onClick={incrementHandler}><Smile/></button>
+    </form>
+  );
+};
+
+export default Pray;
